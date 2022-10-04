3a520a4b97f2724e42a440d457865896c7bd66b0
Ayush Bendwal <Bendwalayush3@gmail.com>
2022-10-04 09:49:20 +0530

    third commit

--- main.py
+++ main.py
@@ -8,5 +8,15 @@ def index():
     return "hello world"
 
 
+@app.route("/home")
+def home():
+    return "Home"
+
+
+@app.route("/contact-us")
+def contactUs():
+    return "Contact US"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
